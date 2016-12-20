ce228f3ce8938c74b55b3f53933952ee04bca166
Praneet Sharma <psharma@newrelic.com>
2016-12-19 18:05:28 -0800

    spa api use

--- nr-react.js
+++ nr-react.js
@@ -7,19 +7,28 @@
     console.log('addons missing');
     return;
   }    
-
+  if (typeof NREUM === 'undefined' && typeof NREUM.interaction !== 'function' ) {
+    console.log('api missing');
+    return;
+  }
   function testPerf(){
       // do whatever you like here
+      var myInteraction = newrelic.interaction().setName('tracing').save();
       React.addons.Perf.start();
-
+      console.log('tracing');
       setTimeout(function(){
         React.addons.Perf.stop();
+        console.log('stopping');
         var map = React.addons.Perf.getMeasurementsSummaryMap(React.addons.Perf._allMeasurements);
+        console.log(map.length);
         for (var i = 0; i < map.length; i++) {
           if (typeof NREUM !== 'undefined' && typeof NREUM.addPageAction === 'function' ) {
-            NREUM.addPageAction('react-perf', {Instances: map[i]['Instances'], OwnerComponent: map[i]['Owner > component'], wastedTime: map[i]['Wasted time (ms)']});
+            console.log('sending');
+            myInteraction.setAttribute(map[i]['Owner > component'], map[i]['Wasted time (ms)']);
+            myInteraction.setAttribute('Instances: ' + map[i]['Owner > component'],map[i]['Instances']]);
           }
         }
+        myInteraction.end();
         setTimeout(testPerf, 1000);
 
       }, 10000);
